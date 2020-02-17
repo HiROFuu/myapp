90449c3204015873161e82a76d893abfa462d522
HiROFuu <waikikikun@gmail.com>
2020-02-17 12:37:29 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | 花乃國"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | 花乃國"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | 花乃國"    
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | 花乃國"
+  end
+  
+  test "should get whats Ikebana" do
+    get static_pages_whatsikebana_url
+    assert_response :success
+    assert_select "title", "Whats Ikebana | 花乃國"
   end
 
 end
