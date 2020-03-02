54eb0d6c5c6e09a7dc47d1ec63d593013354c505
HiROFuu <waikikikun@gmail.com>
2020-03-02 12:25:52 +0000

    10章途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
-  def new
-   @user = User.new
-  end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,10 +18,24 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+
   private
+
     def user_params
-        params.require(:user).permit(:name, :email, :password,
-                                     :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
