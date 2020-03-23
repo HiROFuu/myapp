1da51c639532ca17b2d0d5559a14fb85efdd0969
HiROFuu <waikikikun@gmail.com>
2020-03-23 14:21:24 +0000

    3/23

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,14 +5,14 @@ class Event < ApplicationRecord
 
     name_like(search_params[:name])
       .prefecture_like(search_params[:prefecture])
-      .style_is(search_params[:style])
+      .style_like(search_params[:style])
       .from_date(search_params[:from_date])
       .to_date(search_params[:from_date])
 
   end
   
   scope :name_like, -> (name) { where('name LIKE ?', "%#{name}%") if name.present? }
-  scope :style_is, -> (style) { where(style: style) if style.present? }
+  scope :style_like, -> (style) { where('style LIKE ?', "%#{style}%") if style.present? }
   scope :from_date, -> (from_date) { where('from_date <= ?', from_date) if from_date.present? }
   scope :to_date, -> (from_date) { where('to_date >= ?', from_date) if from_date.present? }
   scope :prefecture_like, -> (prefecture) { where('prefecture LIKE ?', "%#{prefecture}%") if prefecture.present? }
