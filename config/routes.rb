d64ee7f0a8f8cd579087a853087229aa013dfcba
HiROFuu <waikikikun@gmail.com>
2020-05-09 09:05:15 +0000

    レイアウト改修中

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
   get 'users/new'
 
-  root 'static_pages#home'
+  root 'static_pages#home2'
 
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
